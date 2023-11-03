89097c10c3e8556e5f7ada3d911d904d6bc20085
suhyeon.park <suhyeon.park@yanolja.com>
2023-11-03 14:43:39 +0900

    :constructor: grid

---
+++ src/components/css/Grid.tsx
@@ -0,0 +1,9 @@
+export const Grid = (): JSX.Element => {
+  return (
+    <div>
+      <div>Grid</div>
+      <div className={'top'}></div>
+      <div className={'bottom'}></div>
+    </div>
+  );
+};
