726a54d2c375d4b9330f91c94b600c510e6ee4ac
suhyeon.park <suhyeon.park@yanolja.com>
2023-11-05 12:59:16 +0900

    :alien: add main route

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,8 +1,12 @@
+import { PageLayout } from 'components/molecules/layout/PageLayout';
+
 const Page = (): JSX.Element => {
   return (
-    <>
-      <p>this is page</p>
-    </>
+    <PageLayout title={'main'}>
+      <p>Hi this is suhyeon.</p>
+      <p>This is a front-end laboratory</p>
+      <p>You can experience it</p>
+    </PageLayout>
   );
 };
 
