e9e607654cffe02862e614b5b69fda16e3219497
suhyeon.park <suhyeon.park@yanolja.com>
2023-12-26 15:29:47 +0900

    react-credit-card-input으로 하면 기존과 같은 방식이라 리펙토링 의미 없음

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       hook: path.resolve(__dirname, 'src/hook'),
       pages: path.resolve(__dirname, 'src/pages'),
       components: path.resolve(__dirname, 'src/components'),
+      utils: path.resolve(__dirname, 'src/utils'),
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
