cbd397571f9e2026bb5294ab07d25e7c41a95bb9
suhyeon.park <suhyeon.park@yanolja.com>
2023-12-27 09:42:45 +0900

    :construction: 비공개 라이브러리라 ㄴㄴ

--- src/components/organism/javascript/CreditCard.tsx
+++ src/components/organism/javascript/CreditCard.tsx
@@ -1,18 +1,11 @@
 import { Block } from 'components/atom/block/Block';
 import { useState } from 'react';
-import { checkIsNumeric, formatCardNumber, reachedMaxLength } from '../../../util/creditCards';
+import { formatCardNumber } from '../../../util/creditCards';
 
 export const CreditCard = () => {
   const [cardNumber, setCardNumber] = useState('');
 
   const handleChangeCardNumbers = (e: any) => {
-    const val = e.target.value;
-    if (val) {
-      const valueLength = val.split('-').join('').length;
-      if (reachedMaxLength(val, valueLength)) {
-        e.preventDefault();
-      }
-    }
     setCardNumber(e.target.value);
   };
 
