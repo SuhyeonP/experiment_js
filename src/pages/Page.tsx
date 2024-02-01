cc5bf76c7d7babaf2c34f7705fd2da5eff2b7e25
suhyeon.park <suhyeon.park@yanolja.com>
2024-02-01 09:52:54 +0900

    :pencil2: fix spell

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -3,7 +3,7 @@ import { PageLayout } from 'components/molecules/layout/PageLayout';
 const Page = (): JSX.Element => {
   return (
     <PageLayout title={'main'}>
-      <p>Hi this is suhyeon.</p>
+      <p>Hi this is Suhyeon.</p>
       <p>This is a front-end laboratory</p>
       <p>You can experience it</p>
     </PageLayout>
